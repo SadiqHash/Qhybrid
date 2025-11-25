14c4e1cf1057743261df54c80b34939c120d7970
SadiqHash <saddeequjp@gmail.com>
2025-11-26 00:24:23 +0100

    Add minimal boilerplate stubs for translator.py and create __init__.py for quantum

---
+++ quantum/translator.py
@@ -0,0 +1,7 @@
+class Translator:
+    """Stub translator between Qiskit and Cirq representations."""
+    def qiskit_to_cirq(self, qiskit_circuit):
+        raise NotImplementedError("qiskit_to_cirq not implemented yet.")
+
+    def cirq_to_qiskit(self, cirq_circuit):
+        raise NotImplementedError("cirq_to_qiskit not implemented yet.")
